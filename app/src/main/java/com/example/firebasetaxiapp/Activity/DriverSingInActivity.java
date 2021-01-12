07dc057e06c90fa35debd2c1e2ee96809326517f
Renlov <a.pimenov95@mail.ru>
2021-01-12 21:15:03 +0300

    auth method

--- app/src/main/java/com/example/firebasetaxiapp/Activity/DriverSingInActivity.java
+++ app/src/main/java/com/example/firebasetaxiapp/Activity/DriverSingInActivity.java
@@ -1,17 +1,26 @@
 package com.example.firebasetaxiapp.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.firebasetaxiapp.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class DriverSingInActivity extends AppCompatActivity {
+
+    private static final String TAG = "DriverSingInActivity";
 
     private TextInputLayout textInputEmail;
     private TextInputLayout textInputName;
@@ -22,6 +31,8 @@ public class DriverSingInActivity extends AppCompatActivity {
     private TextView toggleLoginSingUpTextView;
 
     private boolean isLoginModeActive;
+
+    private FirebaseAuth auth;
 
 
     @Override
@@ -36,6 +47,8 @@ public class DriverSingInActivity extends AppCompatActivity {
 
         loginSingUpButton = findViewById(R.id.loginSingUpButton);
         toggleLoginSingUpTextView = findViewById(R.id.toggleLoginSingUpTextView);
+
+        auth = FirebaseAuth.getInstance();
     }
 
     private boolean validateEmail() {
@@ -90,14 +103,57 @@ public class DriverSingInActivity extends AppCompatActivity {
 
 
     public void loginSingUpUser(View view) {
-        if(!validateEmail() | !validateName() | !validatePassword()) {
+        if (!validateEmail() | !validateName() | !validatePassword()) {
             return;
         }
 
-        String userInput = "Email: " + textInputEmail.getEditText().getText().toString().trim() +
-                "\n" + "Name: " + textInputName.getEditText().getText().toString().trim() +
-                "\n" + "Password: " + textInputPassword.getEditText().getText().toString().trim();
-        Toast.makeText(this, userInput, Toast.LENGTH_SHORT).show();
+        if (isLoginModeActive) {
+            auth.signInWithEmailAndPassword(textInputEmail.getEditText().getText().toString().trim(),
+                    textInputPassword.getEditText().getText().toString().trim())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInWithEmail:success");
+                                FirebaseUser user = auth.getCurrentUser();
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInWithEmail:failure", task.getException());
+                                Toast.makeText(DriverSingInActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                                // ...
+                            }
+
+                            // ...
+                        }
+                    });
+        } else {
+
+            auth.createUserWithEmailAndPassword(textInputEmail.getEditText().getText().toString().trim(),
+                    textInputPassword.getEditText().getText().toString().trim())
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = auth.getCurrentUser();
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(DriverSingInActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+        }
     }
 
     public void toggleLoginSingUp(View view) {
