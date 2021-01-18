070306795d21a7a776f860c26158638a61041482
Renlov <a.pimenov95@mail.ru>
2021-01-18 23:30:54 +0300

    fix

--- app/src/main/java/com/example/firebasetaxiapp/Activity/DriverMapsActivity.java
+++ app/src/main/java/com/example/firebasetaxiapp/Activity/DriverMapsActivity.java
@@ -1,29 +1,92 @@
 package com.example.firebasetaxiapp.Activity;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.example.firebasetaxiapp.BuildConfig;
 import com.example.firebasetaxiapp.R;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
 
 public class DriverMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
+    private static final int REQUEST_LOCATION_PERMISSION = 333;
+    private static final int CHECK_SETTING_CODE = 111;
+
+    //Класс для определения местоположения
+    private FusedLocationProviderClient fusedLocationClient;
+    //Для доступа к настройкам
+    private SettingsClient settingsClient;
+    //Для сохранения данных запроса
+    private LocationRequest locationRequest;
+    //Для определения настроек устройства
+    private LocationSettingsRequest locationSettingsRequest;
+    //Используется для событий определения местоположения
+    private LocationCallback locationCallback;
+    //Класс, в котором храниться широта и долгота
+    private Location currentLocation;
+
+    private boolean isLocationActive;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_driver_maps);
+
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        settingsClient = LocationServices.getSettingsClient(this);
+
+        buildLocationRequest();
+        buildLocationCallBack();
+        buildLocationSettingRequest();
+
+        startLocationUpdate();
     }
 
     /**
@@ -40,8 +103,210 @@ public class DriverMapsActivity extends FragmentActivity implements OnMapReadyCa
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng driverLocation = new LatLng(-34, 151);
+        mMap.addMarker(new MarkerOptions().position(driverLocation).title("Driver Location"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(driverLocation));
+    }
+    private void stopLocationUpdates() {
+
+        if(!isLocationActive) {
+            return;
+        }
+
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        isLocationActive = false;
+
+                    }
+                });
+    }
+
+
+    private void startLocationUpdate() {
+
+        isLocationActive = true;
+
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+                    @Override
+                    public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                        if (ActivityCompat.checkSelfPermission(DriverMapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                            return;
+                        }
+                        fusedLocationClient.requestLocationUpdates(locationRequest,
+                                locationCallback, Looper.myLooper());
+                    }
+                }).addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode) {
+                    case LocationSettingsStatusCodes.RESOLUTION_REQUIRED:
+                        try {
+                            ResolvableApiException resolvableApiException =
+                                    (ResolvableApiException) e;
+                            resolvableApiException.startResolutionForResult(
+                                    DriverMapsActivity.this, CHECK_SETTING_CODE
+                            );
+                        } catch (IntentSender.SendIntentException sendIntentException) {
+                            sendIntentException.printStackTrace();
+                        }
+                        break;
+
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:
+                        String message = "Make location settings on your device";
+                        Toast.makeText(DriverMapsActivity.this, message, Toast.LENGTH_SHORT).show();
+                        isLocationActive = false;
+
+                }
+                updateLocationUi();
+            }
+        });
+    }
+
+    private void buildLocationSettingRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(locationRequest);
+        locationSettingsRequest = builder.build();
+    }
+
+    private void buildLocationCallBack() {
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                currentLocation = locationResult.getLastLocation();
+
+                updateLocationUi();
+            }
+        };
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case CHECK_SETTING_CODE:
+                switch (resultCode) {
+                    case Activity
+                            .RESULT_OK:
+                        Log.d("MainActivity", "User has agreed to change location" + "settings");
+                        startLocationUpdate();
+                        break;
+
+                    case Activity.RESULT_CANCELED:
+                        Log.d("MainActivity", "User has not agreed to change location" + "settings");
+                        isLocationActive = false;
+                        updateLocationUi();
+                        break;
+
+                }
+                break;
+        }
+    }
+
+    private void updateLocationUi() {
+
+        if (currentLocation != null) {
+            LatLng driverLocation = new LatLng(currentLocation.getLatitude(),
+                    currentLocation.getLongitude());
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(driverLocation));
+            mMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+            mMap.addMarker(new MarkerOptions().position(driverLocation).title("Driver Location"));
+        }
+    }
+
+    private void buildLocationRequest() {
+
+        locationRequest = new LocationRequest();
+        locationRequest.setInterval(10000);
+        locationRequest.setFastestInterval(1000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (isLocationActive && checkLocationPermission()) {
+            startLocationUpdate();
+        } else if (!checkLocationPermission()) {
+            requestLocationPermission();
+        }
+    }
+
+    private boolean checkLocationPermission() {
+        int permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
+    }
+
+    private void requestLocationPermission() {
+        boolean shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        if (shouldProvideRationale) {
+            showSnackBar("Location permission in needed for work",
+                    "OK", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            ActivityCompat.requestPermissions(
+                                    DriverMapsActivity.this, new String[] {
+                                            Manifest.permission.ACCESS_FINE_LOCATION
+                                    },
+                                    REQUEST_LOCATION_PERMISSION
+                            );
+
+                        }
+                    });
+        } else {
+            ActivityCompat.requestPermissions(
+                    DriverMapsActivity.this, new String[] {
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                    },
+                    REQUEST_LOCATION_PERMISSION );
+
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_LOCATION_PERMISSION) {
+            if (grantResults.length <= 0){
+                Log.d("onRequestPermissions", "Request was cancelled");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                if(isLocationActive) {
+                    startLocationUpdate();
+                }
+            } else {
+                showSnackBar("Turn on location on settings", "Settings",
+                        new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent intent = new Intent();
+                                intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                                Uri uri = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null);
+                                intent.setData(uri);
+                                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                                startActivity(intent);
+                            }
+                        });
+            }
+        }
+    }
+
+    private void showSnackBar(final String mainText, final String action, View.OnClickListener listener) {
+        Snackbar.make(findViewById(android.R.id.content),
+                mainText, Snackbar.LENGTH_INDEFINITE).setAction(action, listener)
+                .show();
+
     }
 }
